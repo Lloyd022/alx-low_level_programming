e58f21f694dabb3ceee7eb90199f96997aa6a6ab
Lloyd022 <hendrickmalatji017@gmail.com>
2022-07-21 04:26:08 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- *  * length - function that returns the length of a string
- *   * @s: string
- *    * Return: length of a string
- *     */
+ * length - function that returns the length of a string
+ * @s: string
+ * Return: length of a string
+ */
 int length(char *s)
 {
-		if (*s != '\0')
-				{
-							return (1 + length(s + 1));
-								}
-			return (0);
+if (*s != '\0')
+{
+return (1 + length(s + 1));
+}
+return (0);
 }
 /**
  *  * checker - helper function
@@ -20,28 +20,28 @@ int length(char *s)
  *      */
 int checker(char *s, int x)
 {
-		if (x <= 0)
-				{
-							return (1);
-								}
-			if (*s == *(s + x - 1))
-					{
-								return (checker(s + 1, x - 2));
-									}
-				else
-						{
-									return (0);
-										}
+if (x <= 0)
+{
+return (1);
+}
+if (*s == *(s + x - 1))
+{
+return (checker(s + 1, x - 2));
+}
+else
+{
+return (0);
+}
 }
 /**
- *  * is_palindrome - function that returns 1 if a string is a palindrome
- *   * and 0 if not.
- *    * @s: string
- *     * Return: 1 if a string is a palindrome and 0 if not
- *      */
+ * is_palindrome - function that returns 1 if a string is a palindrome
+ * and 0 if not.
+ * @s: string
+ * Return: 1 if a string is a palindrome and 0 if not
+ */
 int is_palindrome(char *s)
 {
-		int x = length(s);
+int x = length(s);
 
-			return (checker(s, x));
+return (checker(s, x));
 }
