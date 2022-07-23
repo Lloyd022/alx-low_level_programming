b426c9a6a2b727110ad0b4e113de03d4c68ff0f7
Lloyd022 <hendrickmalatji017@gmail.com>
2022-07-23 16:49:27 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -28,28 +28,24 @@ coins++;
 if ((cents - 25) >= 0)
 {
 cents -= 25;
-																				continue;
-																				}
+continue;																			}
 if ((cents - 10) >= 0)
 {
-																				cents -= 10;
-																				continue;
-			
+cents -= 10;
+continue;
 }
 if ((cents - 5) >= 0)
 {
-																				cents -= 5;
-																				continue;
-																				}
+cents -= 5;
+continue;
+}
 if ((cents - 2) >= 0)
-																				{
-																				cents -= 2;
-																				continue;
-																				}
-									
-
+{
+cents -= 2;
+continue;
+}
 cents--;
-																				}
+}
 
 printf("%d\n", coins);
 
