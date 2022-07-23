9cf2d5ed3d30320aaac41b74fb1341c124c55d97
Lloyd022 <hendrickmalatji017@gmail.com>
2022-07-23 16:20:26 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,7 +6,7 @@
  *    * @argv: An array of pointers to the arguments.
  *     *
  *      * Return: Always 0.
- *       */
+ */
 int main(int argc, char __attribute__((__unused__)) *argv[])
 {
 printf("%d\n", argc - 1);
